e331b1d3b7810e231b91cf43846d27dd18c2482f
zhaobw <1964471757@qq.com>
2019-07-17 14:58:32 +0800

    实现简易tapbale

---
+++ 1.case.js
@@ -0,0 +1,22 @@
+class SyncHook { // 钩子是同步的
+    constructor(args){
+        this.tasks = [];
+    }
+    tap(name,task){
+        this.tasks.push(task);
+    }
+    call(...args){
+        this.task.forEach((task)=>task(...args));
+    }
+}
+
+let hook = new SyncHook(['name']);
+
+hook.tap('react',function(name){
+    console.log('react',name);
+});
+hook.tap('node',function(name){
+    console.log('node',name);
+});
+
+hook.call('jw');
